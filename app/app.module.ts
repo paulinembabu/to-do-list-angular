704fec81b82d2c72cf8a4c6536d18f1af7f15363
paulinembabu <paulinembabu@gmail.com>
2017-01-13 09:24:44 +0300

    initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }   from './app.component';
+import { PiesListComponent } from './pies-list.component';
+import { TaskListComponent } from './task-list.component';
+import { EditTaskComponent } from './edit-task.component';
+import { NewTaskComponent } from './new-task.component';
+import { CompletenessPipe } from './completeness.pipe';
+import { TaskComponent } from './task.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    PiesListComponent,
+    TaskListComponent,
+    EditTaskComponent,
+    NewTaskComponent,
+    CompletenessPipe,
+    TaskComponent,
+  ],
+  bootstrap:    [ AppComponent ]
+})
+
+export class AppModule { }
